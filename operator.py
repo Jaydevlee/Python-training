d3521ab01c3135b39960cc696bfbbe79a1c5ff1e
Jaydevlee <yousick5@gmail.com>
2026-01-05 10:49:38 +0900

    first commit

---
+++ operator.py
@@ -0,0 +1,10 @@
+#연산자는 다른 언어와 다르지 않으나 산술연산자는 Python만 가지고 있는 연산자가 있다.
+num1=2
+num2=3
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)   #곱셈
+print(num1 ** num2)   #제곱
+print(num1 / num2)  #자바에서는 몫만 출력하나 파이썬에서는 연산 결과 전체를 출력
+print(num1 % num2)
+print(num1 // num2)  #자바와 같은 결과를 출력
---
+++ start.py
@@ -0,0 +1,8 @@
+# <- 주석을 의미
+#System.out.println() = print()
+print('Hello, World')
+print("Hello, World")
+
+#변수선언 키워드나, 타입이 없다.
+name = "홍길동"
+print(name)
