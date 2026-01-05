ce572b8968d4091cc502d2f54ca322ac132a20ab
Jaydevlee <yousick5@gmail.com>
2026-01-05 16:05:02 +0900

    delete unaccessary file

---
+++ arr.py
@@ -0,0 +1,12 @@
+# list - 자바의 배열과 동일, 요소의 값 추가, 수정 가능
+arr1 = ["닥터스트레인지", "스플릿", "기생충"]
+print(arr1)
+print(arr1[1])
+arr1.append("배트맨")
+print(arr1)
+# tuple - 자바의 배열과 유사, 요소의 값 추가, 수정 불가능
+arr2 = ("닥터스트레인지", "스플릿", "기생충")
+print(arr2)
+print(arr2[1])
+arr2.append("배트맨")
+print(arr2)
